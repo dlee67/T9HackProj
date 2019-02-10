ac114d0a2de155315a719e99d2d7e66ed9fe0cb6
dlee67 <dlee67@msudenver.edu>
2019-02-09 17:41:27 -0700

    I did something significant.

--- app/src/main/java/com/example/bob/t9hackproj/MainActivity.kt
+++ app/src/main/java/com/example/bob/t9hackproj/MainActivity.kt
@@ -1,59 +1,39 @@
 package com.example.bob.t9hackproj
 
-import android.app.Activity
-import android.content.Intent
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-// You won't be able to find an official documentation for the below package because
-// it was developed by a third party library.
-// https://github.com/firebase/FirebaseUI-Android
-// The above link contains the doc for the below package.
-import com.firebase.ui.auth.AuthUI
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.auth.FirebaseAuth
-import java.util.*
+import android.widget.ImageView
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FileDownloadTask
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.File
+import java.io.IOException
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var mFirebaseAnalytics: FirebaseAnalytics
-    lateinit var providers: List<AuthUI.IdpConfig>
-    lateinit var uID: String
-    val memeForActivity = 1
-    val LOG_TAG = MainActivity::class.java.getSimpleName()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
-        providers = Arrays.asList(
-                AuthUI.IdpConfig.EmailBuilder().build()
-        )
-        initFirebase()
-    }
-
-    private fun initFirebase() {
-
-        uID = FirebaseAuth.getInstance().uid!!
-        Log.i(LOG_TAG, "uID: " + uID)
-
-        if(uID == null) {
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .build(), memeForActivity
-            )
+        var storage = FirebaseStorage.getInstance()
+        var storageRef = storage.getReferenceFromUrl("gs://t9hackproj.appspot.com/")
+                .child("GoroAndAoba.jpg")
+        var animeStuff = findViewById<ImageView>(R.id.aoba_and_goro)
+        try{
+            var localFile = File.createTempFile("images", "jpg ");
+            storageRef.getFile(localFile).addOnSuccessListener (
+                OnSuccessListener<FileDownloadTask.TaskSnapshot>(){
+                    Log.i("dhl", "In the block of OnSuccessListener.")
+                    var bitMap = BitmapFactory.decodeFile(localFile.absolutePath)
+                    animeStuff.setImageBitmap(bitMap)
+                })
+        }catch(e: IOException){
+            Log.i("dhl", "Everything breaks!");
         }
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(requestCode == memeForActivity && resultCode == Activity.RESULT_OK){
-            uID = FirebaseAuth.getInstance().uid!!
-            Log.i(LOG_TAG, "New uID: " + uID);
-        }
     }
 }
